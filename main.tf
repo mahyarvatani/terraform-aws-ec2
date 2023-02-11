fa60470f8faac32be539b89a395a3d5ff9fc7f12
unknown <mahyar.vatani@yahoo.com>
2023-02-11 13:07:52 +0330

    vpc and subnet

--- main.tf
+++ main.tf
@@ -1,9 +1,22 @@
-resource "aws_instance" "web" {
-  ami           = "ami-0aa7d40eeae50c9a9"
-  instance_type = "t2.micro"
-  key_name      = "mhv"
+variable "env-name" {}
+variable "vpc-cider-block" {}
+variable "subnet-cidr-block" {}
+variable "subnet-availability-zone" {}
+
+resource "aws_vpc" "my-vpc" {
+  cidr_block = var.vpc-cider-block
+
   tags = {
-    Name = "web"
-    Created_by = "terraform"
+    Name: "${var.env-name}-vpc"
+  }
+}
+
+resource "aws_subnet" "my-first-subnet" {
+  vpc_id            = aws_vpc.my-vpc.id
+  cidr_block        = var.subnet-cidr-block
+  availability_zone = var.subnet-availability-zone
+
+  tags = {
+    Name: "${var.env-name}-subnet"
   }
 }
