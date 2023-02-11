2ffcaee031c96666fd527fef377176cbe26dbd95
unknown <mahyar.vatani@yahoo.com>
2023-02-11 17:58:22 +0330

    route table & igw & subnet association

--- main.tf
+++ main.tf
@@ -20,3 +20,30 @@ resource "aws_subnet" "my-first-subnet" {
     Name: "${var.env-name}-subnet"
   }
 }
+
+resource "aws_internet_gateway" "my-vpc-igw" {
+
+  vpc_id = aws_vpc.my-vpc.id
+  tags ={
+    Name: "${var.env-name}-igw"
+  }
+  
+}
+
+resource "aws_route_table" "my-vpc-rtb" {
+  vpc_id = aws_vpc.my-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my-vpc-igw.id
+  }
+}
+
+resource "aws_route_table_association" "rtb-to-subnet" {
+
+  route_table_id = aws_route_table.my-vpc-rtb.id
+  subnet_id = aws_subnet.my-first-subnet.id
+  
+}
+
+
+
